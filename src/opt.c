0f378f4f39cd620c1fbeff07bbe5903c400d1698
Philippe CHADENIER <pchadeni@e1r10p13.42.fr>
2018-01-31 13:19:36 +0100

    Final ?

--- src/opt.c
+++ src/opt.c
@@ -6,38 +6,51 @@
 /*   By: pchadeni <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/24 11:24:38 by pchadeni          #+#    #+#             */
-/*   Updated: 2018/01/29 15:25:27 by pchadeni         ###   ########.fr       */
+/*   Updated: 2018/01/31 10:09:31 by pchadeni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minish.h"
 
-void	replace_ls(t_line *env, char **cmd)
+static void	modify_one_cmd(t_line *env, char **cmd, int i)
 {
 	t_line	*pwd;
 	t_line	*oldpwd;
+	t_line	*home;
+
+	pwd = get_smtg(env, "PWD");
+	oldpwd = get_smtg(env, "OLDPWD");
+	home = get_smtg(env, "HOME");
+	if (ft_strequ(cmd[i], "~+") && pwd)
+	{
+		ft_strdel(&cmd[i]);
+		cmd[i] = ft_strdup(pwd->value);
+	}
+	else if (ft_strequ(cmd[i], "~-") && oldpwd)
+	{
+		ft_strdel(&cmd[i]);
+		cmd[i] = ft_strdup(oldpwd->value);
+	}
+	else if (ft_strequ(cmd[i], "~") && home)
+	{
+		ft_strdel(&cmd[i]);
+		cmd[i] = ft_strdup(home->value);
+	}
+}
+
+void		replace_ls(t_line *env, char **cmd)
+{
 	int		i;
 
 	i = 0;
-	pwd = get_smtg(env, "PWD");
-	oldpwd = get_smtg(env, "OLDPWD");
 	while (cmd[i])
 	{
-		if (ft_strequ(cmd[i], "~+") && pwd)
-		{
-			ft_strdel(&cmd[i]);
-			cmd[i] = ft_strdup(pwd->value);
-		}
-		if (ft_strequ(cmd[i], "~-") && oldpwd)
-		{
-			ft_strdel(&cmd[i]);
-			cmd[i] = ft_strdup(oldpwd->value);
-		}
+		modify_one_cmd(env, cmd, i);
 		i++;
 	}
 }
 
-uint8_t	can_access(void)
+uint8_t		can_access(void)
 {
 	char	*try;
 	int		res;
